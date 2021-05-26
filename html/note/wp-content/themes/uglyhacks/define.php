3c5683c63c2cad1a84175dba81e17952727380f0
aonuma-mori <aonuma.mori@gmail.com>
2021-05-26 14:15:15 +0900

    heeader整備

--- html/note/wp-content/themes/uglyhacks/define.php
+++ html/note/wp-content/themes/uglyhacks/define.php
@@ -4,3 +4,5 @@ define('PREFIX', "uh");
 define('BLOG_TITLE', get_bloginfo('name')." (アグリーハックス)");
 define('DESCRIPTION', "WEB制作に関連する話題を扱う技術ブログです。PHP,Python,AWS,GCP,Javascript,Vue.jsなどモダンなWEB開発を中心に記事を書いております。");
 define('OG_IMAGE_PATH', get_template_directory_uri()."/assets/img/og-uglyhacks.png");
+define('BLOG_CSS_PATH', "/note/wp-content/themes/uglyhacks/assets/css/main.css");
+define('BLOG_JS_PATH', "/note/wp-content/themes/uglyhacks/assets/js/main.js");
--- html/note/wp-content/themes/uglyhacks/index.php
+++ html/note/wp-content/themes/uglyhacks/index.php
@@ -5,7 +5,7 @@ include("define.php");
 include("block/common.php");
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="ja">
   <head prefix="og: https://ogp.me/ns# fb: https://ogp.me/ns/fb# website: https://ogp.me/ns/<?php echo $blog_page_type; ?>#">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,8 +31,10 @@ include("block/common.php");
 
     <link rel="canonical" href="<?php echo $blog_permalink; ?>">
 
-    <link rel="stylesheet" href="（cssのURL）" media="screen">
-    <script type="text/javascript" src="（jsのURL）"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css"> -->
+    <link rel="stylesheet" href="<?php echo BLOG_CSS_PATH; ?>" media="screen">
+    <script type="text/javascript" src="<?php echo BLOG_JS_PATH; ?>"></script>
 
     <link rel="icon" href="favicon.ico">
     <link rel="apple-touch-icon-precomposed" href="（アップルタッチアイコンの画像URL）">
@@ -43,9 +45,58 @@ include("block/common.php");
   <body>
     <!-- （googleアナリティクスを入れる箇所②） -->
     <header>
+      <div class="break-point-bar"></div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand site-title" href="<?php echo $blog_home_url; ?>"><h1 class="site-title">Uglyhacks</h1></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown link
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <form class="d-flex align-items-end pc">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-secondary" type="submit">
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+              </svg>
+            </button>
+          </form>
+        </div>
+      </nav>
     </header>
     <main>
-      <h1><a href="<?php echo $blog_home_url; ?>">uglyhacks</a></h1>
+
+    <form class="d-flex align-items-end sp">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-secondary" type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+        </svg>
+      </button>
+    </form>   
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           <div class="article-contents mb30">
             <div class="fs18 index-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
