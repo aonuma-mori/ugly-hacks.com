8c47f1af92bf47962e1da26728cec15d91483f9a
aonuma-mori <aonuma.mori@gmail.com>
2021-06-07 11:54:51 +0900

    いろいろ

--- html/note/wp-content/themes/uglyhacks/functions.php
+++ html/note/wp-content/themes/uglyhacks/functions.php
@@ -16,4 +16,37 @@ add_action( 'after_setup_theme', 'my_theme_setup');
 if ( function_exists('register_sidebar') ) register_sidebar();
 
 
+/* Media html tag */
+function image_wrap($html, $id, $caption, $title, $align, $url, $size, $alt){
+  $html = <<< EOM
+  <!-- Button trigger modal -->
+  <a data-bs-toggle="modal" data-bs-target="#modalid_{$id}">
+    <img src="{$url}" width="100%" alt="{$title}">
+  </a>
+  <!-- Modal -->
+  <div class="modal fade" id="modalid_{$id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">{$alt}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <img src="{$url}" width="100%" alt="{$title}">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+EOM;
+  $html = preg_replace("/\n/","", $html);
+  $html = preg_replace("/\s+/"," ", $html);
+  $html = preg_replace("/\s+/"," ", $html);
+  $html = '<div id="imageblock" class="imageblock col-sm-12 col-md-12 col-lg-6">'."\n".$html."\n".'</div>'."\n";
+	return $html;
+}
+add_filter('image_send_to_editor','image_wrap',10,8);
+
 
--- html/note/wp-content/themes/uglyhacks/single.php
+++ html/note/wp-content/themes/uglyhacks/single.php
@@ -1,14 +1,71 @@
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <h1 class="h1 article-title"><?php the_title(); ?></h1>
-    <div class="text-right fs11 mb10 mr10"><?php the_time('Y.n.j (D)'); ?></div>
-    <div class="fs13 wp-contents">
-      <?php the_content(); ?>
-      <br clear="both">
+<?php
+include("define.php");
+include("block/common.php");
+?>
+<?php get_header(); ?>
+
+
+<?php
+  /**
+   * カテゴリーを取得
+   */
+  $category = get_the_category();
+  $cat = $category[0];
+  $cat_name = $cat->name;
+  $cat_id = $cat->cat_ID;
+  $cat_slug = $cat->slug;
+  $cat_link = esc_url(get_category_link($cat_id));
+
+  // echo $cat_name;
+  // echo $cat_link; 
+?>
+
+<div class="container page-header">
+	<p class="breadcrumb">
+		<span class="page-out">
+			<a href="/note/">Home</a>
+		</span>&nbsp;>&nbsp;
+		<span class="page-scope">
+    <a href="<?php echo $cat_link; ?>"><?php echo $cat_name; ?></a>
+    </span>&nbsp;>&nbsp;
+    <span class="page-scope">
+      <?php echo mb_substr(get_the_title(),0,12)."..."; ?>
+    <span>
+	</p>
+
+<div class="container article-block">
+  <div class="row">
+    <div class="col-sm-7 col-md-8 col-lg-9 main">
+      <div class="single-article">
+        <h2><?php echo the_title(); ?></h2>
+        <div class="single-article-text">
+          <?php the_content(); ?>
+          <br clear="both">
+          <?php the_time('Y.n.j (D)'); ?>
+        </div>
+        <div class="post_meta clearfix">
+          <div class="category"><?php the_category(); ?></div>
+          <div class="tags"><?php the_tags("<span class='tag'>","","</span>"); ?></div>
+        </div>
+      </div>
     </div>
-    <div class="post_meta clearfix">
-      <div class="category"><?php the_category(); ?></div>
-      <div class="tags"><?php the_tags("<span class='tag'>","","</span>"); ?></div>
+    <div class="col-sm-5 col-md-4 col-lg-3 sidebar">
+      <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
+      <?php endif; ?>
     </div>
-<?php endwhile; else: ?>
-    <div>記事がありません。</div>
-<?php endif; ?>
+  </div>
+</div>
+
+
+
+
+
+</main><!-- header-include -->
+  <footer>
+    <div class="copyright">
+      <p class="text-center">(c) 2021 uglyhacks.com All rights reserved.</p>
+    </div>
+  </footer>
+<script type="text/javascript" src="（ここにjsのURL）"></script>
+</body>
+</html>
