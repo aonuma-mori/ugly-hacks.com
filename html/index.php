1ea4230b821a105d1b510a8caea94523dd0c6b29
aonuma-mori <aonuma.mori@gmail.com>
2021-07-08 11:42:44 +0900

    トップページのパラメータ修正

--- html/index.php
+++ html/index.php
@@ -61,14 +61,14 @@ $tw = @file_get_contents("tw.txt");
   <div class="break-point-bar"></div>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand site-title" href="<?php echo $parameter['ug_url']; ?>"><h1 class="site-title">Uglyhacks</h1></a>
+      <a class="navbar-brand site-title" href="<?php echo $parameter['ug_url']; ?>/"><h1 class="site-title">Uglyhacks</h1></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>    
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo $ug_meta_og_url; ?>/note/">Blog</a>
+            <a class="nav-link active" aria-current="page" href="<?php echo $parameter['ug_url']; ?>/note/">Blog</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo $parameter['ug_url'].$parameter['ug_archives_page']; ?>">Archives</a>
