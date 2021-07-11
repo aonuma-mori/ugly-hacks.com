1f227fb967030912ca016853b6c3169c3fd62338
aonuma-mori <aonuma.mori@gmail.com>
2021-07-11 12:13:30 +0900

    twitteer RSSで画像投稿だけ取得するを追加

--- html/twitter_rss.php
+++ html/twitter_rss.php
@@ -1,4 +1,5 @@
 <?php
+require 'config.php';
 require_once 'vendor/autoload.php';
 
 use Monolog\Logger;
@@ -34,7 +35,8 @@ if ($old_twitter_rss != $twitter_rss) {
 }
 
 
-
+$re = $twitter->fetchImagemedia();
+// var_dump($re);
 
 
 
@@ -44,10 +46,18 @@ if ($old_twitter_rss != $twitter_rss) {
  */
 class ServiceTwitterRssController
 {
+  
+  // public $twitter_rss_url = "https://twitter-great-rss.herokuapp.com/feed/user?name=GGM06605451&url_id_hash=197dd4a8ee90b6867995b397dd28e37501a406db";
+  //public $twitter_rss_url = "https://twitter-great-rss.herokuapp.com/feed/user?name=aonuma_moriri&url_id_hash=197dd4a8ee90b6867995b397dd28e37501a406db";
   public $twitter_rss_url = "http://twitter-great-rss.herokuapp.com/feed/user?name=a141828410&url_id_hash=3d0bcd52ad998ad6ed1b72d816af4d04544cb26b";
   public $rss;
   public $tweets = [];
+  public $images = [];
 
+  /**
+   * para 0で最新のツィート1件を取得する
+   * 
+   */
   public function getFreshRSS($index=NULL)
   {
     if (@file_get_contents($this->twitter_rss_url)) {
@@ -66,6 +76,7 @@ class ServiceTwitterRssController
          * @userへの返信はスキップ
          * url付きの投稿はスキップ
          * 画像投稿は削除
+         * 
          */
         if (preg_match("/'http:\/\/pbs.twimg.com\/media\//",$tweet)) {  
           continue;
@@ -96,4 +107,57 @@ class ServiceTwitterRssController
 
     return $this->tweets;
   }
+
+  /**
+   * Tweetの画像投稿を取得する
+   * @userへの返信はスキップ
+   * url付きの投稿はスキップ
+   * テキスト投稿スキップ
+   * 
+   */
+  public function fetchImagemedia()
+  {
+    $tw = $this->fetchRSS();
+    foreach($tw->channel as $v) {
+      
+      foreach ($v as $item) {
+        $tweet = $item->description[0];
+        /**
+         * iconの除去
+         * @userへの返信はスキップ
+         */
+        $tweet = preg_replace("/<img src='http:\/\/pbs.twimg.com\/profile_images\/.*\/.*_normal.jpg' width='16px' height='16px' \/>/","",$tweet);
+        if (preg_match("/@.*/",$tweet)) {  
+          continue;
+        }
+        $img_pattern = '/<img.*?src\s*=\s*[\"|\'](.*?)[\"|\'].*?>/i';
+        $img = preg_match($img_pattern, $tweet, $images);
+        if (!empty($images[0])) {
+          array_push($this->images, $images[0]);
+        }
+      }
+    }
+    return $this->images;
+  }
+
+  /**
+   * Twitter Great RSSからRSSを取り出す
+   * 
+   * Twitter Great RSS
+   * @url: https://twitter-great-rss.herokuapp.com/
+   */
+  public function fetchRSS()
+  {
+    if (@file_get_contents($this->twitter_rss_url)) {
+      $rss = file_get_contents($this->twitter_rss_url);
+      $this->rss = simplexml_load_string($rss);
+    } else {
+      /* Defaultの文言追加 */
+      $this->rss = "ツィートがありません。";
+    }
+    return $this->rss;
+  }
+
+
+
 }
