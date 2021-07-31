269d78d27e522b5c746ac6c4fcf4377070641f3e
aonuma-mori <aonuma.mori@gmail.com>
2021-07-31 12:20:16 +0900

    wordpress pagemnations

--- html/note/wp-content/themes/uglyhacks/front-page.php
+++ html/note/wp-content/themes/uglyhacks/front-page.php
@@ -57,6 +57,20 @@ include("block/common.php");
 					<?php if( function_exists("the_pagination") ) the_pagination(); ?>
 			</div>
 
+			<div class="top-pagenation">
+				<?php the_posts_pagination(
+					array(
+							'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
+							'prev_next'     => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
+							'prev_text'     => __( 'Forward'), // 「前へ」リンクのテキスト
+							'next_text'     => __( 'Backward'), // 「次へ」リンクのテキスト
+							'type'          => 'list', // 戻り値の指定 (plain/list)
+							'screen_reader_text'=>' ',
+					)
+				);
+				?>
+			</div>
+
 
 		<!-- <div class="container top-article-block">
 			<div class="row">
